a2bcc1a42393aad2de19364377be60975ca0f48e
phbo85 <p.bochmann@googlemail.com>
2022-03-01 10:51:22 +0100

    add favicon

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import Head from 'next/head';
 import {
   Stack,
   Checkbox,
@@ -38,56 +39,66 @@ export default function Home() {
   }, [value, showDate, showCountdown]);
 
   return (
-    <Center
-      bgImage={'url(background.svg)'}
-      bgSize="cover"
-      bgRepeat="no-repeat"
-      minH="100vh"
-      w="full"
-    >
-      <Box
-        backdropFilter="auto"
-        backdropBlur="8px"
-        backdropBrightness="70%"
-        rounded="3xl"
-        padding="4"
-        maxW="xl"
-      >
-        <Heading as="h1" mb="4" textAlign="center">
-          ⌛ discord timestamp maker ⌛
-        </Heading>
-        <Text color="gray.200" fontSize="sm">
-          Enter a time in natural language to create timestamps (e.g. "5pm",
-          "tomorrow 9pm", "8 UTC" or "10:30 IST")
-        </Text>
-        <Input
-          value={value}
-          onChange={handleChange}
-          placeholder="enter time in natural language"
-          rounded="lg"
-          p="2"
-          size="lg"
-          my="4"
+    <>
+      <Head>
+        <title>Discord Timestamp Maker</title>
+
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>⌛</text></svg>"
         />
-        <Text>
-          Result: <strong>{parsedTime}</strong>
-        </Text>
-        <Output output={output} />
-        <Stack spacing={5} direction="row" mt="2">
-          <Checkbox
-            isChecked={showDate}
-            onChange={(e) => setShowDate(e.target.checked)}
-          >
-            Show date
-          </Checkbox>
-          <Checkbox
-            isChecked={showCountdown}
-            onChange={(e) => setShowCountdown(e.target.checked)}
-          >
-            Show countdown
-          </Checkbox>
-        </Stack>
-      </Box>
-    </Center>
+      </Head>
+      <Center
+        bgImage={'url(background.svg)'}
+        bgSize="cover"
+        bgRepeat="no-repeat"
+        minH="100vh"
+        w="full"
+      >
+        <Box
+          backdropFilter="auto"
+          backdropBlur="8px"
+          backdropBrightness="70%"
+          rounded="3xl"
+          padding="4"
+          maxW="xl"
+        >
+          <Heading as="h1" mb="4" textAlign="center">
+            ⌛ Discord Timestamp Maker ⌛
+          </Heading>
+          <Text color="gray.200" fontSize="sm">
+            Enter a time in natural language to create timestamps (e.g. "5pm",
+            "tomorrow 9pm", "8 UTC" or "10:30 IST")
+          </Text>
+          <Input
+            value={value}
+            onChange={handleChange}
+            placeholder="enter time in natural language"
+            rounded="lg"
+            p="2"
+            size="lg"
+            my="4"
+          />
+          <Text>
+            Result: <strong>{parsedTime}</strong>
+          </Text>
+          <Output output={output} />
+          <Stack spacing={5} direction="row" mt="2">
+            <Checkbox
+              isChecked={showDate}
+              onChange={(e) => setShowDate(e.target.checked)}
+            >
+              Show date
+            </Checkbox>
+            <Checkbox
+              isChecked={showCountdown}
+              onChange={(e) => setShowCountdown(e.target.checked)}
+            >
+              Show countdown
+            </Checkbox>
+          </Stack>
+        </Box>
+      </Center>
+    </>
   );
 }
