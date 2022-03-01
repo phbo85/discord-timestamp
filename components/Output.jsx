fe23225233c94c1ce3e92be5709b1ba1134a3f66
phbo85 <p.bochmann@googlemail.com>
2022-03-01 10:43:52 +0100

    add app

---
+++ components/Output.jsx
@@ -0,0 +1,23 @@
+import {
+  Button,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Text,
+  useClipboard,
+} from '@chakra-ui/react';
+
+export default function Output({ output }) {
+  const { hasCopied, onCopy } = useClipboard(output);
+
+  return (
+    <InputGroup>
+      <Input value={output} isReadOnly />
+      <InputRightElement width="4.5rem">
+        <Button disabled={!output} onClick={onCopy} h="1.75rem" size="sm">
+          {hasCopied ? 'Copied' : 'Copy'}
+        </Button>
+      </InputRightElement>
+    </InputGroup>
+  );
+}
